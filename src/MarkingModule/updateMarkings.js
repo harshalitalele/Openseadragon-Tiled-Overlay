162b9151bc1bade82051791efe9b8a60c84ac4c1
harshalitalele <harshalitalele@gmail.com>
2019-04-23 18:48:09 +0530

    update markings

--- src/MarkingModule/updateMarkings.js
+++ src/MarkingModule/updateMarkings.js
@@ -4,16 +4,32 @@
 
 var TiledOverlay = (function() {
     
+    var drawTiles = {};
+    
+    function activeImg(img, level, x, y) {
+        this.img = img;
+        this.level = level;
+        this.x = x;
+        this.y = y;
+    }
+    
     function updateOverlay(needToShow, overlay, osd) {
         if(needToShow) {
             overlay.show();
+            // get viewport limits
+            // get viewport limits intersection markings (from actual schema which lies in current viewport)
+            // draw canvas
         } else {
             overlay.hide();
         }
     }
     
+    function drawCanvas(img, x, y) {
+        //
+    }
+    
     return function(osd, getUrl, schema) {
-        var overlay = new Overlay('openseadragon-overlay', osd),
+        var overlay = new Overlay('openseadragon-tiled-overlay', osd),
             isVisible = true;
         
         overlay.elem.style.pointerEvents = 'none';
